5c56b4a41de9fa09ca647df395857679bd6d9e98
Oleksandr Krol <you@example.com>
2020-07-15 13:32:30 +0300

    Opimize Coordinates::adjust_coordinates: return text_chunk_t() before getting start/end_coordinates
    Signed-off-by: Oleksandr Krol <you@example.com>

--- coordinates.cc
+++ coordinates.cc
@@ -83,10 +83,10 @@ text_chunk_t Coordinates::adjust_coordinates(string &&s, size_t len, float width
         if (c == ' ') x += Tw * Th;
     }
     const matrix_t T_end = translate_matrix(m, x, y);
-    const pair<float, float> start_coordinates = apply_matrix_pt(T_start, 0, ty);
-    const pair<float, float> end_coordinates = apply_matrix_pt(T_end, adv, ty + fonts.get_height() * Tfs);
     x += adv;
     if (prev_f != f) return text_chunk_t();//do not render vertical fonts
+    const pair<float, float> start_coordinates = apply_matrix_pt(T_start, 0, ty);
+    const pair<float, float> end_coordinates = apply_matrix_pt(T_end, adv, ty + fonts.get_height() * Tfs);
     float x0 = min(start_coordinates.first, end_coordinates.first);
     float x1 = max(start_coordinates.first, end_coordinates.first);
     float y0 = min(start_coordinates.second, end_coordinates.second);
